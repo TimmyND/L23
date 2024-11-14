ebba895a9f7a8bed05eeb0ad7187998666cd9555
Timothy Diehl <diehltimmy@gmail.com>
2024-11-14 17:59:23 -0500

    Changed the frequency

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import math
 
 def display_plot():
     X = [x/10 for x in range(100)]
-    plt.plot(X, [math.sin(x) for x in X])
+    plt.plot(X, [math.sin(0.25*x) for x in X])
     plt.show()
 
 display_plot()
